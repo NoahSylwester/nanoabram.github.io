f5fb88748e5a5d16cafdb2b92f35444fabd0092b
Noah Sylwester <nanoabram@comcast.net>
2019-06-14 02:41:52 -0700

    My third.

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("/Users/Noah/nsylwester.github.io")
+setwd("/Users/Noah/Desktop/nsylwester.github.io")
 
 #render your sweet site. 
 rmarkdown::render_site()
